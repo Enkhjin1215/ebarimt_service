8cbe567c978e948ad18992a836fa00c0ee295c12
Khebe <khebe@infinite.mn>
2024-04-30 11:25:44 +0800

    ignored

--- producer/dist/ebarimt/ebarimt.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { EbarimtRequest } from './model';
-import { EbarimtService } from './ebarimt.service';
-export declare class EbarimtController {
-    private ebarimtService;
-    constructor(ebarimtService: EbarimtService);
-    createRequest(ebarimtRequest: EbarimtService): Promise<EbarimtRequest>;
-    getOrders(): import("rxjs").Observable<any>;
-    getList(): Promise<EbarimtRequest[]>;
-}
